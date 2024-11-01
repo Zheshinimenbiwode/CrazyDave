8538a469d05579a852005e164e7e5645b6808de9
Zheshinimenbiwode <2900345105@qq.com>
2024-11-01 15:09:55 +0800

    1

--- 1/app/src/main/java/com/example/crazydave/user_info_page.java
+++ 1/app/src/main/java/com/example/crazydave/user_info_page.java
@@ -14,7 +14,7 @@ public class user_info_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.user_info);
+        setContentView(R.layout.user_info_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.user_info), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
